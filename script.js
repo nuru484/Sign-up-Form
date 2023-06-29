7b10d4e82303026a5dc4cd127455f55c8975b918
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2023-06-29 05:54:12 -0800

    Added some more css code for easy responsiveness and some validations

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+
+password.addEventListener('change', validatePasswordMatch);
+confirmPassword.addEventListener('input', validatePasswordMatch);
+
+function validatePasswordMatch() {
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.setCustomValidity('Passwords do not match ooh');
+    console.log('hello 1');
+  } else {
+    confirmPassword.setCustomValidity('');
+    console.log('hello other');
+  }
+}
