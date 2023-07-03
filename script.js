c52386865d7b09c6f4eaac029a7fff8cb6e44bd4
Nurudeen Abdul-Majeed <abdulmajeednurudeen48@gmail.com>
2023-07-03 07:07:51 -0800

    Added some javascript code to validate the password and confirm password match and also added a bit of css code to make to look nice on mobiles, though not responsive enough

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
+const passwordRequiredNumber =
+  document.getElementsByClassName('passwordNotMatch');
 
-password.addEventListener('change', validatePasswordMatch);
+password.addEventListener('input', validatePasswordMatch);
 confirmPassword.addEventListener('input', validatePasswordMatch);
 
 function validatePasswordMatch() {
   if (password.value !== confirmPassword.value) {
-    confirmPassword.setCustomValidity('Passwords do not match ooh');
-    console.log('hello 1');
+    confirmPassword.setCustomValidity('Passwords do not match');
+    passwordRequiredNumber.append('Passwords do not match');
   } else {
     confirmPassword.setCustomValidity('');
-    console.log('hello other');
+    confirmPassword.style.cssText = 'border: none;';
+    passwordRequiredNumber.textContent = '';
   }
 }
